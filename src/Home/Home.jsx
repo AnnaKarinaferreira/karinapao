ba9b0099342f645a98d6e9631cf718b18eca41c4
annapacheco <annapacheco203@gmail.com>
2024-10-18 15:37:07 -0300

    deixa de ser preguiçosa ebota msg boa pro commit

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -3,28 +3,33 @@ import React from 'react'
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './Home.css';
+import img01 from './img01.png';
+
 
 const Home = () => {
     return (
-        <>
-        <div className='Header'>
-            <a href="">aaa</a>
-            <a href="">aaa</a> 
-        </div>
-
+    <>
+    <div className='geral'>
+           <div className='alinhamentolinks'>
+           <p className='link1'><a href="">Portfolio Senai</a></p>
+            <p className='link2'><a href="">Meus Projetos</a></p>
+            </div> 
             <div className='container'>
-                <div className='coluna'>
-                    <div className='linha'>
-                        <p className='titulonome '>A N N A</p>
-                    </div>
-                    <div className='linha'>
-                        <p className='subtitulonome'>PACHECO</p>
-                    </div>
+            
+            <div className='coluna'>
+                <div className='linha'>
+                    <p className='titulonome '>A N N A</p>
                 </div>
-                <div className='coluna'>
-                    <img src="" alt="assfdsdfsdg" className='imagem' />
+                <div className='linha'>
+                    <p className='subtitulonome'>PACHECO</p>
                 </div>
             </div>
+            <div className='coluna'>
+                <img src={img01} alt="assfdsdfsdg" className='imagem' />
+            </div>
+        </div>
+        </div>
+           
         </>
     )
 }
