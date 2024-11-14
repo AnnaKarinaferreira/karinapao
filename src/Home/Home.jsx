6f8a05f7f02c1d32b57dd4c5b1f97e582db65103
anna <annapacheco203@gmail.com>
2024-11-14 08:21:10 -0300

    commit changes

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -8,38 +8,70 @@ import img01 from './img01.png';
 
 const Home = () => {
     return (
-    <>
-    <section>
-    <div className='geral'>
-           <div className='alinhamentolinks'>
-           <p className='link1'><a href="">Portfolio Senai</a></p>
-            <p className='link2'><a href="">Meus Projetos</a></p>
-            </div> 
-            <div className='container'>
-            
-            <div className='coluna'>
-                <div className='linha'>
-                    <p className='titulonome '>A N N A</p>
-                </div>
-                <div className='linha'>
-                    <p className='subtitulonome'>PACHECO</p>
-                </div>
-            </div>
-            <div className='coluna'>
-                <img src={img01} alt="assfdsdfsdg" className='imagem' />
-            </div>
-        </div>
-        </div>
-    </section>
+        <>
+            <section>
+                <div className='geral'>
+                    <div className='alinhamentolinks'>
+                        <p className='link1'><a href="">Portfolio Senai</a></p>
+                        <p className='link2'><a href="">Meus Projetos</a></p>
+                    </div>
+                    <div className='container'>
 
-    <section>
-        <p className='sobremim'>Sobre mim...</p>
-        
-    </section>
+                        <div className='coluna'>
+                            <div className='linha'>
+                                <p className='titulonome '>A N N A</p>
+                            </div>
+                            <div className='linha'>
+                                <p className='subtitulonome'>PACHECO</p>
+                            </div>
+                        </div>
+                        <div className='coluna'>
+                            <img src={img01} alt="assfdsdfsdg" className='imagem' />
+                        </div>
+                    </div>
+                </div>
+            </section>
+                <hr />
+            <section>
+                <p className='sobremim'>Sobre mim...</p>
 
-    <section>
-    <p className='sobremim'>Skills</p>
-    </section>
+            </section>
+
+            <section>
+                <p className='sobremim'>Skills</p>
+                <ol>
+                    <li >
+                        <div class="icon"><i class="fa-solid fa-bicycle"></i></div>
+                        <div class="title">Step 1</div>
+                        <div class="descr">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis, porro.</div>
+                    </li>
+                    <li >
+                        <div class="icon"><i class="fa-solid fa-car"></i></div>
+                        <div class="title">Step 2</div>
+                        <div class="descr">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis, porro.</div>
+                    </li>
+                    <li >
+                        <div class="icon"><i class="fa-solid fa-helicopter"></i></div>
+                        <div class="title">Step 3</div>
+                        <div class="descr">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis, porro.</div>
+                    </li>
+                    <li >
+                        <div class="icon"><i class="fa-solid fa-plane"></i></div>
+                        <div class="title">Step 4</div>
+                        <div class="descr">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis, porro.</div>
+                    </li>
+                    <li >
+                        <div class="icon"><i class="fa-solid fa-rocket"></i></div>
+                        <div class="title">Step 5</div>
+                        <div class="descr">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis, porro.</div>
+                    </li>
+                    <li >
+                        <div class="icon"><i class="fa-solid fa-bus"></i></div>
+                        <div class="title">Step 6</div>
+                        <div class="descr">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis, porro.</div>
+                    </li>
+                </ol>
+            </section>
         </>
     )
 }
